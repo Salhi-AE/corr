dfd87e2391c42e31289cd54ecb9225d945f6f9f8
Salhi Abdo <salhiabdennour58@gmail.com>
2025-11-12 23:13:45 +0100

    Blue - refactoring

--- src/note_rate.py
+++ src/note_rate.py
@@ -1,6 +1,5 @@
 def rate_note(note: int) -> str:
-    if note == 7:
-        return "unsuccessful"
-    if note == 9 or note == 8:
+
+    if note < 10:
         return "unsuccessful"
     return "acceptable"
--- tests/test_note_rate.py
+++ tests/test_note_rate.py
@@ -1,7 +1,7 @@
 from src.note_rate import rate_note
 import pytest
 
-@pytest.mark.parametrize("note",[7,8,9])
+@pytest.mark.parametrize("note",[0,1,2,3,4,5,6,7,8,9])
 
 def test_rate_note_unsuccessful(note):
    assert  rate_note(note) == "unsuccessful"
